5e50b21759569054ace38cad6e7fa122214484ea
Joan938 <martinezibarrondojoan@gmail.com>
2025-08-17 19:24:00 -0400

    Add main script and schools_by_topic module for school data management

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function to find schools by topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic
+    
+    Args:
+        mongo_collection: pymongo collection object
+        topic (string): topic searched
+    
+    Returns:
+        List of schools that have the specified topic
+    """
+    return list(mongo_collection.find({"topics": topic}))
